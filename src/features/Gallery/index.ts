3820b005c80d1bf42147eefd26671db66d989790
mateo <matheo.developer@gmail.com>
2022-08-11 20:32:52 -0500

    to creating the screens

---
+++ src/features/Gallery/index.ts
@@ -0,0 +1,3 @@
+export {default as DetailScreen } from './Detail/Detail'
+export {default as HomeScreen } from './Home/Home'
+export {default as ProfileScreen } from './Profile/Profile'
---
+++ src/features/Gallery/screens/Profile/Profile.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+export const ProfileScreen = ()=> <Text> Im Profile </Text>
