d8a1f9d49d468a103b85479b9c1d77a1cda060fa
mateo <matheo.developer@gmail.com>
2022-08-14 16:14:30 -0500

    navigation animation fade

--- src/features/Gallery/navigation/Stack.tsx
+++ src/features/Gallery/navigation/Stack.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Animated} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Routes} from './routes';
 import {DetailScreen, HomeScreen, ProfileScreen} from '../screens';
@@ -7,17 +8,54 @@ import {Header} from '../../../components/Header';
 const Stack = () => {
   const {Navigator, Screen} = createStackNavigator();
 
+  const forSlide = ({current, next, inverted, layouts: {screen}}) => {
+    const progress = Animated.add(
+      current.progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1],
+        extrapolate: 'clamp',
+      }),
+      next
+        ? next.progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1],
+            extrapolate: 'clamp',
+          })
+        : 0,
+    );
+
+    return {
+      cardStyle: {
+        transform: [
+          {
+            translateX: Animated.multiply(
+              progress.interpolate({
+                inputRange: [0, 1, 2],
+                outputRange: [screen.width, 0, screen.width * -0.3],
+                extrapolate: 'clamp',
+              }),
+              inverted,
+            ),
+          },
+        ],
+      },
+    };
+  };
   return (
     <Navigator initialRouteName={Routes.HOME_GALLERY}>
       <Screen
         name={Routes.HOME_GALLERY}
         component={HomeScreen}
-        options={{header: () => <Header typeHeader="Home" />}}
+        options={{
+          cardStyleInterpolator: forSlide,
+          header: () => <Header typeHeader="Home" />,
+        }}
       />
       <Screen
         name={Routes.DETAIL_IMAGE}
         component={DetailScreen}
         options={{
+          cardStyleInterpolator: forSlide,
           header: ({navigation}) => (
             <Header typeHeader="Detail" navigation={navigation} />
           ),
@@ -27,6 +65,7 @@ const Stack = () => {
         name={Routes.PROFILE_GALLERY}
         component={ProfileScreen}
         options={{
+          cardStyleInterpolator: forSlide,
           header: ({navigation}) => (
             <Header typeHeader="Profile" navigation={navigation} />
           ),
